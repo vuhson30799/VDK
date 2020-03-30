02c85edfc5f7ac053922696ca78c1d85453d2084
HOANGSON <vuhson30799@gmail.com>
2020-03-30 19:10:47 +0700

    initial project

---
+++ src/main/java/packages/config/WebInitializer.java
@@ -0,0 +1,22 @@
+package packages.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
